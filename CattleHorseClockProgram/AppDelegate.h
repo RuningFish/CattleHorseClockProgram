5bd7f75974f9a989f56f5b1d5e141d1a0dec8302
runingfish <1826931541@qq.com>
2025-06-03 00:09:44 +0800

    初始化

---
+++ CattleHorseClockProgram/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  CattleHorseClockProgram
+//
+//  Created by runingfish on 2025/6/2.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
